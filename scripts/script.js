27e6a9c473153ff6bee022e6bac09b7bb89c818c
Neozus <114140035+Neozus@users.noreply.github.com>
2022-10-04 15:59:17 +0200

    Small change

---
+++ scripts/script.js
@@ -0,0 +1,75 @@
+/*let myAlphabet = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+    "$",
+    "*",
+    "/",
+    "-",
+    "+",
+  ];
+function myAlphabetlength () {
+    console.log(myAlphabet.length)
+
+}
+myAlphabetlength(); 
+*/
+
+/*let planets = ['Mars', 'Venus','Jupiter','Terre','Mercure'] ;
+planets.forEach(elements => console.log(elements));
+console.log(planets);*/
+
+/*let someDataTypes = ['Terre',22,true,undefined ]
+someDataTypes.forEach(element => { console.log(typeof element)
+    
+});
+console.log(someDataTypes);
+*/
+
+/*
+let Urban = [];
+console.log(Urban);
+let copyUrban = Urban.slice();
+copyUrban.push('nike', 'lacoste');
+console.log(copyUrban); 
+*/
+/*let furnitures = ['Table', 'Chairs','Couch'];
+furnitures.forEach(element => { console.log(element.split(""))
+    
+});
+*/
+    
+
+
+    
+
+
+    
+
+
+
+
+
